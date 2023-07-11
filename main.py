1622f71f294de5ad41e6e5834107736717ab213b
KG-07 <shahriyorbaysariev1007@gmail.com>
2023-07-11 17:45:53 +0500

    modified:   main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from typing import List
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 
@@ -46,3 +48,21 @@ def change_user_name(user_id:int,new_name:str):
     current_user = list(filter(lambda user:user.get("id")==user_id,fake_users2))[0]
     current_user["name"] = new_name
     return{"status":200, "data":current_user}
+
+
+class Trade(BaseModel):
+    id:int
+    user_id:int
+    currency: str
+    side: str
+    price: float
+    amount:float
+
+
+
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return{"status":200, "data":fake_trades}
